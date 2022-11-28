fc7f05c942c402b30e3944399bde0b7037c28f8f
Pooja Raut <110257862+PoojaRaut2512@users.noreply.github.com>
2022-11-29 00:32:27 +0530

    Add files via upload

---
+++ ConnectionProviderCode.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ConnectionProvider;
+
+import java.beans.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Dell
+ */
+public class ConnectionProviderCode 
+{
+    public static Connection getcon()
+    {
+        try
+        {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/StudentResultSystem","root","Pooja@2512");
+            return con;
+        }
+        catch(Exception e)
+        {
+            JOptionPane.showMessageDialog(null, e.toString());
+            return null;
+        }
+    }
+    
+    public static void main(String args[])
+    {
+        ConnectionProviderCode con= new ConnectionProviderCode();
+        Connection c=con.getcon();
+    }
+
+    public Statement createStatement() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+}
